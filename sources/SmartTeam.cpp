4e25c0622c00a59dbcd230f7a792d5ecb56bca3d
barmud3 <130641348+barmud3@users.noreply.github.com>
2023-05-24 20:59:53 +0300

    Update SmartTeam.cpp

--- sources/SmartTeam.cpp
+++ sources/SmartTeam.cpp
@@ -24,54 +24,39 @@ SmartTeam::SmartTeam(SmartTeam* team):Team(team->leader)
 
 void SmartTeam::attack(Team* enemyTeam)
 {
-    if (enemyTeam == nullptr)
+if (enemyTeam == nullptr)
     {
         __throw_invalid_argument("nullptr as team pointer");
     }
+    
     if (this->stillAlive() == 0 || enemyTeam->stillAlive() == 0)
     {
-        throw("All team players are dead");
+        __throw_runtime_error("All team players are dead");
     }
-    
+
     //determine new leader
     if (!this->leader->isAlive())
     {
         this->leader = newLeader();
     }
-    
+    //determine target
     Character* target = this->target(enemyTeam);
-    Character* ourWarriorUnderAttack = enemyTeam->target(this);
-    if (dynamic_cast<Ninja*>(ourWarriorUnderAttack))
-    {
-        
-    }
+    //loop for ninja
     for(Character* warrior : this->warriors)
     {
-        
         if (warrior->isAlive())
         {
-            //if warrior is cowboy.
-            if (dynamic_cast<Cowboy*>(warrior))
+            //if warrior is ninja.
+            if (dynamic_cast<Ninja*>(warrior))
             {
-                Cowboy* cowboyW = ((Cowboy*)warrior);
-                if (cowboyW->hasboolets())
-                {
-                    cowboyW->shoot(target);
-                }
-                else
-                {
-                    cowboyW->reload();
-                }   
-            }
-            //if warrior is ninja
-            else if(dynamic_cast<Ninja*>(warrior))
-            {
+
+                //if warrior is ninja
                 Ninja* NinjaW = ((Ninja*)warrior);
                 if (NinjaW->slash(target) == false) // didn't succes to slash - too far
                 {
                     NinjaW->move(target);
                 }
-            }
+            } 
             //target died from Character attack.
             if (!target->isAlive())
             {
@@ -84,6 +69,39 @@ void SmartTeam::attack(Team* enemyTeam)
             }
         }
     }
+    
+    //loop for cowboy
+    for(Character* warrior : this->warriors)
+    {
+        if (warrior->isAlive())
+        {
+            //if warrior is cowboy.
+            if (dynamic_cast<Cowboy*>(warrior))
+            {
+                Cowboy* cowboyW = ((Cowboy*)warrior);
+
+                if (cowboyW->hasboolets())
+                {
+                    cowboyW->shoot(target);
+                }
+                else
+                {
+                    cowboyW->reload();
+                }   
+            }
+
+            //target died from Character attack.
+            if (!target->isAlive())
+            {
+                //all enemies are died
+                if (enemyTeam->stillAlive() == 0)
+                {
+                    return;
+                }
+                target = this->target(enemyTeam);
+            }
+        }
+    }   
 }
 
 Point SmartTeam::bestPoint(Team* enemyTeam , Character* warrior)
@@ -99,6 +117,18 @@ void SmartTeam::print()
 {
     for(Character* warrior : warriors)
     {
+        //if warrior is ninja.
+        if (dynamic_cast<Ninja*>(warrior))
+        {
         cout << warrior->print() << endl;
+        }
+    }
+        for(Character* warrior : warriors)
+    {
+        //if warrior is cowboy.
+        if (dynamic_cast<Cowboy*>(warrior))
+        {
+        cout << warrior->print() << endl;
+        }
     }
 }
